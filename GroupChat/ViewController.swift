14ef983435ca418a252dd0d6ca2174803bc72ea7
Akan <23879@iitu.kz>
2019-11-22 22:28:10 +0600

    Initial Commit

---
+++ GroupChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupChat
+//
+//  Created by Akan Akysh on 11/22/19.
+//  Copyright © 2019 Akysh Akan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
